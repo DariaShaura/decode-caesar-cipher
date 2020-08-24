e7a38f8a11b2f2393b482adceb443911171100b1
Daria <dar.shaura@gmail.com>
2020-08-24 13:33:02 +0400

    added empty methods in DecodingService.java

--- src/com/company/service/DecodingService.java
+++ src/com/company/service/DecodingService.java
@@ -1,4 +1,17 @@
 package com.company.service;
 
 public class DecodingService {
+
+    private void decodeText(StringBuilder text, int shift){
+        //TODO
+    }
+
+    private boolean isRightDecoding(StringBuilder decodedText){
+        //TODO
+        return false;
+    }
+
+    public void decodeTextFromFile(String fileName){
+        //TODO
+    }
 }
